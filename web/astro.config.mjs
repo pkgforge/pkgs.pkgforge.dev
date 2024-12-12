e6e3c32747ac84faee388ff3eb926ad8b8f155ac
AHQ Miness <ahqsecret@gmail.com>
2024-12-12 19:10:27 +0530

    Use @ahqsoftwares SWC instead of Babel

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,16 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
+import react from "astro-react-swc";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({  
-  cacheDir: './.astro',
+export default defineConfig({
+  cacheDir: "./.astro",
   integrations: [
-    react({
-      include: "./src/**/*.tsx",
-    }),
+    react(),
     tailwind({
       applyBaseStyles: true,
     }),
