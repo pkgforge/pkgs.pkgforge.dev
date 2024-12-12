5e019d000d65786d8b5babf0307af698faa0083d
AHQ <ahqsecret@gmail.com>
2024-12-12 07:57:31 +0530

    Update astro.config.mjs

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({  
-  cacheDir: './.astro'
+  cacheDir: './.astro',
   integrations: [
     react({
       include: "./src/**/*.tsx",
