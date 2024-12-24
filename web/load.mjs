1a7e73f2892ddf7f3617a63f90985f847919865c
AHQ Miness <ahqsecret@gmail.com>
2024-12-24 12:29:01 +0530

    New way to make the sites

--- web/load.mjs
+++ web/load.mjs
@@ -1,5 +1,4 @@
-import { mkdirSync, writeFileSync } from "fs";
-import crypto from "node:crypto";
+import { writeFileSync } from "fs";
 
 const edgeX86 = "https://bin.pkgforge.dev/x86_64-Linux/METADATA.AIO.json";
 const edgeArm64 = "https://bin.pkgforge.dev/aarch64-Linux/METADATA.AIO.json";
@@ -10,87 +9,11 @@ const stableArm64 =
   "https://bincache.pkgforge.dev/aarch64-Linux/METADATA.AIO.json";
 
 const community = "https://soarpkgs.pkgforge.dev/metadata/METADATA.json";
-
-const parseIntoValidStuff = (data) => {
-  if (data.startsWith(".")) {
-    return data.replace(".", "dot_");
-  }
-
-  const look = ["[", "{", "}", "]"];
-
-  if (look.some((x) => data.includes(x))) {
-    return crypto.createHash("md5").update(data).digest("hex");
-  }
-
-  return data;
-};
-
-const writeValue = (data, fileHash, branch, arch, pkgs) => {
-  const astroFile = `---
-import Layout from "../../../../../layouts/Layout.astro";
-import App from "../../../../../components/app.tsx";
-
-const data = ${JSON.stringify(data)};
-
-const logs = data.build_log; /*await fetch(data.build_log)
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error(res.status + ": " + res.statusText);
-    }
-    return res.text();
-  })
-  .catch((e) => {
-    return "✖️ Unable to fetch build logs!\\n" + e;
-  });*/
----
-
-<Layout>
-  <App data={data} logs={logs} client:only />
-</Layout>`;
-
-  const pkgFamilyFile = `---
-import Layout from "../../../../../layouts/Layout.astro";
-import App from "../../../../../components/family.tsx";
-
-const familyName = ${JSON.stringify(data.pkg_family)};
-const apps = ${JSON.stringify(
-    pkgs
-      .sort((a, b) => a.name.localeCompare(b.name))
-      .map(({ name, hash }) => ({
-        name,
-        url: `/repo/${branch}/${arch}/${data.pkg_family}/${hash}`,
-      })),
-    null,
-    2
-  )};
----
-
-<Layout>
-  <App apps={apps} name={familyName} client:only />
-</Layout>`;
-
-  mkdirSync(`./src/pages/repo/${branch}/${arch}/${data.pkg_family}`, {
-    recursive: true,
-  });
-  writeFileSync(
-    `./src/pages/repo/${branch}/${arch}/${data.pkg_family}/${fileHash}.astro`,
-    astroFile
-  );
-  writeFileSync(
-    `./src/pages/repo/${branch}/${arch}/${data.pkg_family}/index.astro`,
-    pkgFamilyFile
-  );
-};
 const run = async (url, branch, arch) => {
   /**
    * @type {{ pkg: string, build_date: string, family: string, sha: string, id: string?, name: string, version: string, category: string, size: string, sizeNum: number, type: "base" | "bin" | "pkg" }[]}
    */
   const response = [];
-
-  /**
-   * @type {Map<string, { type: "base" | "bin" | "pkg", index: number }>}
-   */
-  const set = new Map();
 
   /**
    * @type {{ [key: string]: { name: string, hash: string }[] }}
@@ -100,127 +23,78 @@ const run = async (url, branch, arch) => {
   const resp = await fetch(url).then((res) => res.json());
 
   if (branch === "com" || branch == "community") {
-    resp.forEach((data, index) => {
+    resp.forEach((data) => {
       data.pkg_family = data.pkg_id || "community";
-      const fileHash = parseIntoValidStuff(data.pkg);
 
-      response.push({
-        name: data.pkg,
-        pkg: data.pkg,
-        family: data.pkg_family,
-        version: data.version,
-        sha: data.shasum,
-        type: "base",
-        size: data.size,
-        sizeNum: genSize(data.size),
-        category: data.category,
-        id: "N/A",
-        build_date: data.build_date,
-        url: `/${branch}/${arch}/${data.pkg_family}/${fileHash}`,
-        familyUrl: `/${branch}/${arch}/${data.pkg_family}`,
-      });
+      response.push(data);
 
       if (familyMap[data.pkg_family]) {
-        familyMap[data.pkg_family].push({ name: data.pkg, hash: fileHash });
+        familyMap[data.pkg_family].push(data.pkg);
       } else {
-        familyMap[data.pkg_family] = [{ name: data.pkg, hash: fileHash }];
+        familyMap[data.pkg_family] = [data.pkg];
       }
-
-      writeValue(data, fileHash, branch, arch, familyMap[data.pkg_family]);
-      set.set(data.shasum, { type: "base", index });
     });
   } else {
-    resp.base.forEach((data, index) => {
-      const fileHash = parseIntoValidStuff(data.pkg);
-
-      response.push({
-        name: data.pkg,
-        pkg: data.pkg_name,
-        family: data.pkg_family,
-        version: data.version,
-        sha: data.shasum,
-        type: "base",
-        size: data.size,
-        sizeNum: genSize(data.size),
-        category: data.category,
-        id: "N/A",
-        build_date: data.build_date,
-        url: `/${branch}/${arch}/${data.pkg_family}/${fileHash}`,
-        familyUrl: `/${branch}/${arch}/${data.pkg_family}`,
-      });
+    resp.base.forEach((data) => {
+      response.push(data);
 
       if (familyMap[data.pkg_family]) {
-        familyMap[data.pkg_family].push({ name: data.pkg, hash: fileHash });
+        familyMap[data.pkg_family].push(data.pkg);
       } else {
-        familyMap[data.pkg_family] = [{ name: data.pkg, hash: fileHash }];
+        familyMap[data.pkg_family] = [data.pkg];
       }
-
-      writeValue(data, fileHash, branch, arch, familyMap[data.pkg_family]);
-      set.set(data.shasum, { type: "base", index });
     });
-    resp.bin.forEach((data, index) => {
-      const fileHash = parseIntoValidStuff(data.pkg);
-
-      response.push({
-        name: data.pkg,
-        pkg: data.pkg_name,
-        family: data.pkg_family,
-        sha: data.shasum,
-        version: data.version,
-        type: "bin",
-        size: data.size,
-        sizeNum: genSize(data.size),
-        category: data.category,
-        id: "N/A",
-        build_date: data.build_date,
-        url: `/${branch}/${arch}/${data.pkg_family}/${fileHash}`,
-        familyUrl: `/${branch}/${arch}/${data.pkg_family}`,
-      });
+    resp.bin.forEach((data) => {
+      response.push(data);
 
       if (familyMap[data.pkg_family]) {
-        familyMap[data.pkg_family].push({ name: data.pkg, hash: fileHash });
+        familyMap[data.pkg_family].push(data.pkg);
       } else {
-        familyMap[data.pkg_family] = [{ name: data.pkg, hash: fileHash }];
+        familyMap[data.pkg_family] = [data.pkg];
       }
-
-      writeValue(data, fileHash, branch, arch, familyMap[data.pkg_family]);
-      set.set(data.shasum, { type: "bin", index });
     });
-    resp.pkg.forEach((data, index) => {
-      const fileHash = parseIntoValidStuff(data.pkg);
-
-      response.push({
-        name: data.pkg,
-        pkg: data.pkg_name,
-        family: data.pkg_family,
-        version: data.version,
-        sha: data.shasum,
-        type: "pkg",
-        size: data.size,
-        sizeNum: genSize(data.size),
-        category: data.category,
-        id: data.pkg_id,
-        build_date: data.build_date,
-        url: `/${branch}/${arch}/${data.pkg_family}/${fileHash}`,
-        familyUrl: `/${branch}/${arch}/${data.pkg_family}`,
-      });
+    resp.pkg.forEach((data) => {
+      response.push(data);
 
       if (familyMap[data.pkg_family]) {
-        familyMap[data.pkg_family].push({ name: data.pkg, hash: fileHash });
+        familyMap[data.pkg_family].push(data.pkg);
       } else {
-        familyMap[data.pkg_family] = [{ name: data.pkg, hash: fileHash }];
+        familyMap[data.pkg_family] = [data.pkg];
       }
-
-      writeValue(data, fileHash, branch, arch, familyMap[data.pkg_family]);
-      set.set(data.shasum, { type: "pkg", index });
     });
   }
 
-  response.sort((a, b) => a.name.localeCompare(b.name));
+  response.sort((a, b) =>
+    (a.pkg_name || a.pkg).localeCompare(b.pkg_name || b.pkg)
+  );
 
   writeFileSync(
+    `./src/pages/repo/${branch}/${arch}/_family.json`,
+    JSON.stringify(familyMap, null, 2)
+  );
+  writeFileSync(
+    `./src/pages/repo/${branch}/${arch}/_apps.json`,
+    JSON.stringify(response, null, 2)
+  );
+  writeFileSync(
     `./src/metadata_${branch}_${arch}.json`,
-    JSON.stringify(response)
+    JSON.stringify(
+      response.map((data) => ({
+        name: data.pkg_name || data.pkg,
+        pkg: data.pkg,
+        family: data.pkg_family || data.pkg_id,
+        version: data.version,
+        sha: data.shasum,
+        type: "base",
+        size: data.size,
+        sizeNum: genSize(data.size),
+        category: data.category,
+        id: "N/A",
+        build_date: data.build_date,
+        url: `/${branch}/${arch}/${data.pkg_family}/${data.pkg}`,
+        familyUrl: `/${branch}/${arch}/${data.pkg_family}`,
+      }))
+    )
   );
 };
 
--- web/src/components/app.tsx
+++ web/src/components/app.tsx
@@ -4,7 +4,7 @@ import { ExternalLink, Image, LucideTerminalSquare, Package, ScrollText } from "
 import { buttonVariants } from "./ui/button";
 
 interface AppProps {
-  data: { [key: string]: string };
+  data: { [key: string]: any };
   logs: string;
 }
 
@@ -37,6 +37,24 @@ const resolver: { [key: string]: string } = {
   description: "Description",
 };
 
+function Show({ value }: { value: any }) {
+  if (typeof (value) == "object") {
+    if (Array.isArray(value)) {
+      return <div className="flex space-x-2">{value.map((s) => (<Show value={s} key={JSON.stringify(s)} />))}</div>;
+    } else {
+      return <>{JSON.stringify(value)}</>;
+    }
+  } else if (typeof (value) == "string") {
+    if (value.startsWith("http")) {
+      return <a href={value} target="_blank" rel="noreferrer" className="text-blue-500 dark:text-blue-400 hover:text-purple-500 underline underline-offset-4">{value}</a>;
+    } else {
+      return <>{value}</>;
+    }
+  }
+
+  return <>{JSON.stringify(value)}</>;
+}
+
 export default function App({ data, logs: build }: AppProps) {
   return <div className="flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-3 px-5 mt-3 items-stretch pb-4">
     <div className="md:max-w-[90%] rounded-lg flex flex-col flex-1">
@@ -54,9 +72,7 @@ export default function App({ data, logs: build }: AppProps) {
                     {resolver[Key] || Key}
                   </TableCell>
                   <TableCell className="text-wrap break-all whitespace-normal">
-                    {(typeof (Value) == "object" && !Array.isArray(Value)) && JSON.stringify(Value, null, 2)}
-                    {Array.isArray(Value) && Value.join(", ")}
-                    {(typeof (Value) != "object") && (String(Value).startsWith("http") ? <a href={Value} target="_blank" rel="noreferrer" className="text-blue-500 dark:text-blue-400 hover:text-purple-500 underline underline-offset-4">{Value}</a> : Value)}
+                    <Show value={Value} />
                   </TableCell>
                 </TableRow>
               ))
