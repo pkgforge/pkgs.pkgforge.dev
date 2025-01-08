92dd6d9a5fac48fcf91c77173f585216d276b0c4
AHQ Miness <ahqsecret@gmail.com>
2025-01-08 09:00:26 +0530

    Treat repology as tags

--- web/src/components/app.tsx
+++ web/src/components/app.tsx
@@ -26,7 +26,7 @@ const resolver: { [key: string]: ResolverField } = {
   pkg_name: { label: "Package Name", type: "default" },
   pkg_id: { label: "Package ID", type: "default" },
   bsum: { label: "BLAKE3SUM", type: "hash" },
-  repology: { label: "Repology", type: "category" },
+  repology: { label: "Repology", type: "tags" },
   appstream: { label: "AppStream", type: "link" },
   license: { label: "License", type: "default" },
   snapshots: { label: "Snapshots", type: "version" },
