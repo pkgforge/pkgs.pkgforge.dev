f6ddf434099a8a834cb095935104a31a2bc9fe98
AHQ Miness <ahqsecret@gmail.com>
2025-01-09 11:35:03 +0530

    use category instead of arch

--- web/postbuild.mjs
+++ web/postbuild.mjs
@@ -10,10 +10,10 @@ readdirSync("./src/pages/repo/").forEach((repo) => {
     );
 
     apps.forEach((app) => {
-      const [, , , , , , pkg_family, pkg] = app.pkg_webpage.split("/");
+      const [, , , , , category, pkg_family, pkg] = app.pkg_webpage.split("/");
 
       writeFileSync(
-        `./dist/repo/${repo}/${arch}/${pkg_family}/${pkg}/raw.json`,
+        `./dist/repo/${repo}/${category}/${pkg_family}/${pkg}/raw.json`,
         JSON.stringify(app, null, 2)
       );
     });
